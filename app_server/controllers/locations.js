33097c3c8a7c6bf6db289334a772be861e565f86
John Walsh <john.walsh16609@gmail.com>
2016-06-10 23:26:03 +0100

    Small Changes
    Updated controllers, views and layouts of the info to the client

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,31 @@
 /* Grab the 'home' page */
 module.exports.homelist = function (req, res) {
-  res.render('locations-list', { title: 'Home' });
+  res.render('locations-list', {
+    title: 'Reviewer - find a place near you!',
+    pageHeader: {
+      title: 'Reviewer',
+      strapline: ' - Find and review places near you!'
+    },
+    locations: [{
+            name: 'Starcups',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '100m'
+        }, {
+            name: 'Cafe Hero',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 4,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '200m'
+        }, {
+            name: 'Burger Queen',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 2,
+            facilities: ['Food', 'Premium wifi'],
+            distance: '250m'
+        }]
+  });
 };
 
 /* Grab the 'locations' page */
