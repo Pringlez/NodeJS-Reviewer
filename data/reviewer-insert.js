679f54be5aa12eba25667aff992d63b28fa5cf73
John Walsh <john.walsh16609@gmail.com>
2016-06-16 22:06:08 +0100

    Added More Data
    More test data added to insert file

--- data/reviewer-insert.js
+++ data/reviewer-insert.js
@@ -15,12 +15,11 @@ db.getCollection("locations").ensureIndex({
 
 /** locations records **/
 db.getCollection("locations").insert({
-  "_id": ObjectId("57606c656a875f17150287ab"),
   "name": "Starcups",
   "address": "125 High Street, Reading, RG6 1PS",
   "rating": 3,
   "facilities": [
-    "Hot drinks",
+    "Hot Drinks",
     "Food",
     "Premium WiFi"
   ],
@@ -37,6 +36,64 @@ db.getCollection("locations").insert({
 	"days": 'Saturday',
 	"opening": '8:00am',
 	"closing": '5:00pm',
+	"closed": false
+},{
+	"days": 'Sunday',
+	"closed": true
+}]
+});
+
+db.getCollection("locations").insert({
+  "name": "PC Cafe",
+  "address": "4 Avenue, Cattlepark, RG8 1KS",
+  "rating": 4,
+  "facilities": [
+    "Hot drinks",
+    "Food",
+    "Premium WiFi"
+  ],
+  "coords": [
+    -0.9690884,
+    51.455041
+  ],
+  "openingTimes": [{
+	"days": 'Monday - Friday',
+	"opening": '9:00am',
+	"closing": '11:00pm',
+	"closed": false
+},{
+	"days": 'Saturday',
+	"opening": '9:00am',
+	"closing": '9:00pm',
+	"closed": false
+},{
+	"days": 'Sunday',
+	"closed": true
+}]
+});
+
+db.getCollection("locations").insert({
+  "name": "Fast Food",
+  "address": "4 Avenue, Cattlepark, LG8 1TS",
+  "rating": 2,
+  "facilities": [
+    "Soft Drinks",
+    "Hot Food",
+    "Premium WiFi"
+  ],
+  "coords": [
+    -0.9690884,
+    51.455041
+  ],
+  "openingTimes": [{
+	"days": 'Monday - Friday',
+	"opening": '10:00am',
+	"closing": '1:00am',
+	"closed": false
+},{
+	"days": 'Saturday',
+	"opening": '9:00am',
+	"closing": '2:00am',
 	"closed": false
 },{
 	"days": 'Sunday',
