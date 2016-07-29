7775c7e6755d1cfacc175a2331424e5c2ba6849e
John Walsh <john.walsh16609@gmail.com>
2016-07-29 17:47:16 +0100

    Auth Controller & Routes
    Work on controller and routes nearing finish
    Auth endpoints need to be secured

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+// Load environment variables
+require('dotenv').load();
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-require('./app_api/models/db');
 var uglifyJs = require("uglify-js");
 var fs = require('fs');
+var passport = require('passport');
+
+/** Database config & auth script files */
+require('./app_api/models/db');
+require('./app_api/config/passport');
 
 var routes = require('./app_server/routes/index');
 var routesApi = require('./app_api/routes/index');
@@ -49,12 +56,17 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+/** Static folders */
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'app_client')));
 
+/** Init the auth service */
+app.use(password.initialize());
+
 //app.use('/', routes);
 app.use('/api', routesApi);
-// app.use('/users', users);
+//app.use('/users', users);
 
 app.use(function(req, res) {
   res.sendFile(path.join(__dirname, 'app_client', 'index.html'));
